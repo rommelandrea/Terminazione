2a70c7d9dff1566b98f717b8b40851df3d2f10c6
Andrea Romanello <rommelandrea@gmail.com>
2011-10-20 17:47:03 +0200

    sistemato percorso file

--- src/it/andrearomanello/selfprinting/SelfPrinting.java
+++ src/it/andrearomanello/selfprinting/SelfPrinting.java
@@ -4,10 +4,32 @@
  */
 package it.andrearomanello.selfprinting;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author rommel
  */
 public class SelfPrinting {
-    
+    public static void Stampa(){
+        try{
+            FileInputStream fis = new FileInputStream("src/it/andrearomanello/selfprinting/SelfPrinting.java");
+            
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String str;
+            
+            while((str = br.readLine()) != null){
+                System.out.println(str);
+            }
+            
+            in.close();
+            
+        }catch(Exception e){
+            System.out.println("error: "+e.getMessage());
+        }
+    }
 }
